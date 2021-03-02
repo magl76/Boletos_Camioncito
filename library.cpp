90c9befddd62fe386bf425356cf214a16b2472b0
Manuel Gutierrez <malexandergl76@gmail.com>
2021-03-02 03:32:14 -0600

    Ùltimo Commit biblioteca, detalle con sobrecarga de operadores

--- library.cpp
+++ library.cpp
@@ -1,10 +1,25 @@
 #include "library.h"
-#include <string.h>
+#include <string>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
+
+int evaluar_boletos(vector <TBoleto> &vec2,TBoleto &boleto_alipi){
+    int contador;
+    for(int i=0; i=vec2.size(); i++){
+        if(vec2[i] == boleto_alipi){ //miBoletoA.operator==(miBoletoB)
+            contador++;
+        }
+    }
+}
+
+/*bool evaluar_fecha(Tfecha fecha1&, Tfecha fecha2&){
+    if(fecha1 == fecha2){
+        return true;
+    }
+}*/
 
 bool fnbuscar(vector <TBoleto> &vec, function <bool (TBoleto&)> callback){
 
--- library.h
+++ library.h
@@ -1,30 +1,44 @@
 #ifndef BOLETOS_CAMIONCITO_LIBRARY_H
 #define BOLETOS_CAMIONCITO_LIBRARY_H
 
-#include <string.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
 enum Tmes{enero, febrero, marzo, abril ,mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre};
 
-struct Tfecha{
+/*struct Tfecha{
     short dia;
     Tmes mes;
     unsigned anno;
     short hora, min;
-};
+};*/
 
 struct TBoleto{
     string origen, destino;
-    Tfecha fecha;
+    short hora, min;
+    int dia;
+    Tmes mes;
+    int anno;
     int precio;
 };
 
 bool fnbuscar(vector <TBoleto> &vec, function <bool (TBoleto&)> callback);//funcion que recibe UN VECTOR POR REFERENCIA LLAMADO vec, y una funcion tipo BOOL QUE RECIBE COMO PARÀMETRO UN TTICKET POR REFERENCIA Y SE LLAMA CALLBACK
 //esta funcion està lista para recibir cualquier funcion del tipo lambda, la cual serà definida funcionamiento
+
+//sobrecarga de operadores para boleto y fecha
+bool operator==(TBoleto&, TBoleto&);
+//bool operator==(Tfecha&, Tfecha&);
+
+//definicion de sobrecarga para boleto y fecha
+/*bool Tfecha::operator==(Tfecha & f2){
+    return dia==f2.dia && mes==f2.mes && anno==f2.anno && hora==f2.hora && min==f2.min;
+}*/
+bool TBoleto::operator==(TBoleto & b2){
+    return origen==b2.origen && destino==b2.destino &&  dia==b2.dia && mes==b2.mes && anno==b2.anno && hora==b2.hora && min==b2.min;
+}
 
 #endif //BOLETOS_CAMIONCITO_LIBRARY_H
